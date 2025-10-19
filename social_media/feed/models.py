cdbbc75e78ac7886247547bf36db4baed25a58fd
pratikkshinde <pratikofficialpc@gmail.com>
2025-10-19 13:18:47 +0530

    mycomment

--- social_media/feed/models.py
+++ social_media/feed/models.py
@@ -8,3 +8,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.content[:30]}"
